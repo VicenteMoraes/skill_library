9e28bd8839b29800708c297ebdd2e4c78cdd5f09
VicenteMoraes <vicenteromeiromoraes@gmail.com>
2023-01-09 09:20:10 -0300

    Navigation skills

--- behaviours/skills.py
+++ behaviours/skills.py
@@ -18,18 +18,18 @@ POSE = json.loads(os.environ['ROBOT_POSE'])
 class Skills(Node):
     def __init__(self, name: str = f"skills_{NAME}", wait_period: float = 1.0):
         super(Skills, self).__init__(name)
-        self.nav = BasicNavigator()
+        self.nav_controller = BasicNavigator()
         self.initial_pose = PoseStamped()
         self.log_pub = self.create_publisher(String, '/log', 10)
         self.wait_period = wait_period
-        self.waitmsg_sub = None
-        self.sendmsg_pub = None
-        self.timer = None
 
-        self.message = None
+        self.sendmsg_pub = {}
+        self.timer = {}
+        self.waitmsg_sub = {}
+        self.messages = {}
 
         self.initial_pose.header.frame_id = 'map'
-        self.initial_pose.header.stamp = self.nav.get_clock().now().to_msg()
+        self.initial_pose.header.stamp = self.nav_controller.get_clock().now().to_msg()
         self.initial_pose.pose.position.x = POSE['x']
         self.initial_pose.pose.position.y = POSE['y']
         self.initial_pose.pose.position.z = POSE['z']
@@ -38,24 +38,18 @@ class Skills(Node):
         self.initial_pose.pose.orientation.z = POSE['z_rot']
         self.initial_pose.pose.orientation.w = POSE['w_rot']
 
-        self.nav.setInitialPose(self.initial_pose)
+        self.nav_controller.setInitialPose(self.initial_pose)
 
-    def publish_log(self, message: str):
-        now = str(self.get_clock().now())
-        log_msg = String()
-        log_msg.data = f"{now}_{message}"
-        self.log_pub.publish(log_msg)
-
-    async def navigate(self, waypoints: [[float]]):
+    def poselist_to_posestamped(self, *poses):
         goal_poses = []
-        for waypoint in waypoints:
-            quaternion = quaternion_from_euler(0, 0, waypoint[-1])
+        for pose in poses:
+            quaternion = quaternion_from_euler(0, 0, pose[-1])
 
             goal_pose = PoseStamped()
             goal_pose.header.frame_id = 'map'
-            goal_pose.header.stamp = self.nav.get_clock().now().to_msg()
-            goal_pose.pose.position.x = waypoint[0]
-            goal_pose.pose.position.y = waypoint[1]
+            goal_pose.header.stamp = self.nav_controller.get_clock().now().to_msg()
+            goal_pose.pose.position.x = pose[0]
+            goal_pose.pose.position.y = pose[1]
             goal_pose.pose.orientation.x = quaternion[0]
             goal_pose.pose.orientation.y = quaternion[1]
             goal_pose.pose.orientation.z = quaternion[2]
@@ -63,47 +57,61 @@ class Skills(Node):
 
             goal_poses.append(goal_pose)
 
-        self.nav.goThroughPoses(goal_poses)
+        return goal_poses if len(goal_poses) > 1 else goal_poses[0]
+
+    def publish_log(self, message: str):
+        now = str(self.get_clock().now())
+        log_msg = String()
+        log_msg.data = f"{now}_{message}"
+        self.log_pub.publish(log_msg)
+
+    async def navigate(self, goal_poses: [PoseStamped]):
+        self.nav_controller.goThroughPoses(goal_poses)
 
     async def create_wait_message(self, topic: str):
-        self.waitmsg_sub = self.create_subscription(String, topic, self.msg_received, 1)
+        self.waitmsg_sub[topic] = self.create_subscription(String, topic, (lambda msg: self.msg_received(msg, topic)), 1)
+        self.messages[topic] = None
 
-    def msg_received(self, msg):
+    def msg_received(self, msg, topic):
         self.publish_log("message received")
-        self.message = msg.data
+        self.messages[topic] = msg.data
 
-    def msg_not_received(self):
+    def msg_not_received(self, topic):
         self.publish_log("message not received")
-        self.message = False
+        self.messages[topic] = False
 
-    async def wait_message(self, wait_period: float = None):
+    async def wait_for_message(self, topic: str, wait_period: float = None):
         # Blocks until message is received or time limit expires
         wait_period = wait_period if wait_period is not None else self.wait_period
-        self.timer = self.create_timer(wait_period, self.msg_not_received)
+        self.timer = self.create_timer(wait_period, (lambda _: self.msg_not_received(topic)))
 
-        while self.message is None:
+        while self.messages[topic] is None:
             rclpy.spin_once(self)
 
         self.timer.destroy()
         message = self.message or None
-        self.message = None
+        self.messages[topic] = None
         return message
 
     async def send_message(self, topic: str, message: str):
-        self.sendmsg_pub = self.create_publisher(String, topic, 1)
-        self.sendmsg_pub.publish(String(data=f"{message}"))
+        try:
+            self.sendmsg_pub[topic]
+        except KeyError:
+            self.sendmsg_pub = self.create_publisher(String, topic, 1)
+        finally:
+            self.sendmsg_pub.publish(String(data=message))
         self.publish_log("sent message")
 
     async def approach_person(self, goal_pose: PoseStamped):
-        self.nav.goToPose(goal_pose)
+        self.nav_controller.goToPose(goal_pose)
 
     async def approach_robot(self, goal_pose: PoseStamped):
-        self.nav.goToPose(goal_pose)
+        self.nav_controller.goToPose(goal_pose)
 
     async def authenticate_person(self):
         await self.create_wait_message("nurse/fauth")
         await self.send_message("/led_strip/display", message='nurse')
-        auth_msg = await self.wait_message()
+        auth_msg = await self.wait_for_message("nurse/fauth")
         return auth_msg == 'auth'
 
     async def operate_drawer(self):
@@ -135,5 +143,6 @@ if __name__ == "__main__":
         ]
     ]
 
-    asyncio.run(skills.navigate(path))
+    goal_poses = skills.poselist_to_posestamped(path)
+    asyncio.run(skills.navigate(goal_poses))
     rclpy.spin(skills)
